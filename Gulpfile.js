92ce229d0449cb644addeae4d0ddff244ed1b533
Tyler Lawson <tylerjlawson2@gmail.com>
2020-08-22 20:02:04 -0400

    automated print to pdf process

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,25 +1,26 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var sass = require('gulp-sass');
 
 const scssDir = 'assets/css/sass';
-const cssDir  ='assets/css/stylesheets';
+const cssDir = 'assets/css/stylesheets';
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
   return gulp.src(scssDir + '/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(cssDir));
+    .pipe(gulp.dest(cssDir))
+    .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
-gulp.task('serve', function() {
+gulp.task('serve', function () {
   browserSync.init({
     server: '.'
   });
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(scssDir + '/*.scss', gulp.series('sass'));
   gulp.watch('index.html').on('change', browserSync.reload);
 });
--- print.js
+++ print.js
@@ -1,8 +1,21 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('./index.html', 'utf-8');
-var options = { format: 'Letter', border: '0.5in' };
+const pieces = html.split('./');
+html = pieces.join(`file://${process.cwd()}/`);
+const vericalMargin = '0.25in';
+const horizontalMargin = '0.5in';
+var options = {
+  border: {
+    top: vericalMargin,
+    bottom: vericalMargin,
+    left: horizontalMargin,
+    right: horizontalMargin,
+  },
+  height: '11in',
+  width: '8.5in',
+};
 
-pdf.create(html, options).toFile('./test.pdf', function (err, res) {
+pdf.create(html, options).toFile('./Resume.pdf', function (err, res) {
   if (err) return console.log(err);
 });
