a8b8949b83903959df67b9a25126b75501891dc4
Tyler Lawson <tylerjlawson2@gmail.com>
2020-10-08 22:43:34 -0400

    updated styles and markdownify added to points

--- print.js
+++ print.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
-var html = fs.readFileSync('./public/index.html', 'utf-8').replace('/main.css', './public/main.css');
+var html = fs.readFileSync('./public/index.html', 'utf-8')
+             .replace('/main.css', './public/main.css')
+             .replace('</head>', '<style>html{zoom:0.8;}a{font-size: 14px }.wrap{padding: 0;}h1{font-size:45px;}</style></head>');
 const pieces = html.split('./');
 html = pieces.join(`file://${process.cwd()}/`);
 const vericalMargin = '0.25in';
