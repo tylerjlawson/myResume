6b0f1e744d1d80dc8e358a7bc2cbdb171e85449e
Tyler Lawson <tylerjlawson2@gmail.com>
2021-01-18 21:54:56 -0500

    updated resume

--- print.js
+++ print.js
@@ -1,8 +1,12 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
-var html = fs.readFileSync('./public/index.html', 'utf-8')
-             .replace('/main.css', './public/main.css')
-             .replace('</head>', '<style>html{zoom:0.8;}a{font-size: 14px }.wrap{padding: 0;}h1{font-size:45px;}</style></head>');
+var html = fs
+  .readFileSync('./public/index.html', 'utf-8')
+  .replace('main.css', './public/main.css')
+  .replace(
+    '</head>',
+    '<style>html{zoom:0.8;}header a{font-size: 14px }.wrap{padding: 0;}h1{font-size:45px;}</style></head>'
+  );
 const pieces = html.split('./');
 html = pieces.join(`file://${process.cwd()}/`);
 const vericalMargin = '0.25in';
