da4156c4b1f478c44371843e62e6a804cbf45954
Tyler Lawson <tylerjlawson2@gmail.com>
2021-01-21 18:04:27 -0500

    save

--- print.js
+++ print.js
@@ -1,27 +1,27 @@
-var fs = require('fs');
-var pdf = require('html-pdf');
+const fs = require('fs');
+const pdf = require('html-pdf');
 var html = fs
   .readFileSync('./public/index.html', 'utf-8')
   .replace('main.css', './public/main.css')
   .replace(
     '</head>',
-    '<style>html{zoom:0.8;}header a{font-size: 14px }.wrap{padding: 0;}h1{font-size:45px;}</style></head>'
+    '<style>html{zoom:0.8;}header a{font-size:14px;}.wrap{padding: 0;}h1{font-size:45px;}</style></head>'
   );
 const pieces = html.split('./');
 html = pieces.join(`file://${process.cwd()}/`);
-const vericalMargin = '0.25in';
-const horizontalMargin = '0.5in';
-var options = {
+const my = '0.25in';
+const mx = '0.5in';
+const options = {
   border: {
-    top: vericalMargin,
-    bottom: vericalMargin,
-    left: horizontalMargin,
-    right: horizontalMargin
+    top: my,
+    bottom: my,
+    left: mx,
+    right: mx
   },
   height: '11in',
   width: '8.5in'
 };
 
-pdf.create(html, options).toFile('./static/Resume.pdf', function (err, res) {
+pdf.create(html, options).toFile('./static/Resume.pdf', function (err) {
   if (err) return console.log(err);
 });
